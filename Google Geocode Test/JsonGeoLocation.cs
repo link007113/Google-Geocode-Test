9809d4625b2dfd899e989a3f2c5d662424389589
link007113 <43582369+link007113@users.noreply.github.com>
2022-03-28 12:01:38 +0200

    First commit

---
+++ Google Geocode Test/JsonGeoLocation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google_Geocode_Test
+{
+    class JsonGeoLocation
+    {
+        public Result[] results { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Result
+    {
+        public Address_Components[] address_components { get; set; }
+        public string formatted_address { get; set; }
+        public Geometry geometry { get; set; }
+        public string place_id { get; set; }
+        public Plus_Code plus_code { get; set; }
+        public string[] types { get; set; }
+
+        public string google_maps_url
+        {
+            get
+            {
+                return
+                    $"https://www.google.com/maps/search/?api=1&query={geometry.location.lat}%2C{geometry.location.lng}&query_place_id={place_id}";
+            }
+        }
+    }
+
+    public class Geometry
+    {
+        public Location location { get; set; }
+        public string location_type { get; set; }
+        public Viewport viewport { get; set; }
+    }
+
+    public class Location
+    {
+        public float lat { get; set; }
+        public float lng { get; set; }
+    }
+
+    public class Viewport
+    {
+        public Northeast northeast { get; set; }
+        public Southwest southwest { get; set; }
+    }
+
+    public class Northeast
+    {
+        public float lat { get; set; }
+        public float lng { get; set; }
+    }
+
+    public class Southwest
+    {
+        public float lat { get; set; }
+        public float lng { get; set; }
+    }
+
+    public class Plus_Code
+    {
+        public string compound_code { get; set; }
+        public string global_code { get; set; }
+    }
+
+    public class Address_Components
+    {
+        public string long_name { get; set; }
+        public string short_name { get; set; }
+        public string[] types { get; set; }
+    }
+
+}
